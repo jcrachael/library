89c521fc0b0b669fb2b736f529e2a7bcc4df2361
Rachael Cole <jcrachael@gmail.com>
2022-10-23 14:50:13 +1100

    first pass

--- scripts.js
+++ scripts.js
@@ -12,46 +12,72 @@ function Book(title, author, pages, read) {
     }
 }
 
-// Add book to library
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
 // Display books
 function displayBooks() {
     let bookList = document.getElementById('book-list');
     bookList.innerHTML = '';
+    // for each book in the length of my library 
     for (let i = 0; i < myLibrary.length; i++) {
-        bookList.innerHTML += '<li class="card">' + myLibrary[i].info() + '</li>';
-    }
+        // print that book
+        bookList.innerHTML += 
+        `<li class="card" data-index-number="` + i + `">
+            <span class="card-info">` + 
+                myLibrary[i].info() + 
+            `</span>
+            <span class="delete-book" >
+                <img 
+                    src="remove.png" 
+                    alt="Remove book" 
+                    class="delete-book-icon" 
+                    data-index-number="` + i + `" id="` + i + `">
+            </span>
+        </li>`;
+    } 
 }
 
-
-// testing books
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', 295, 'not read yet');
-
-addBookToLibrary(theHobbit);
-
-
-
+function addBook() {
+    // set variables for book properties
+    let newTitle = document.getElementById('book-title').value;
+    let newAuthor = document.getElementById('book-author').value;
+    let newPages = document.getElementById('book-pages').value;
+    let readYet = document.getElementById('read-yet');
+    // set read status depending on option selected
+    let readStatus;
+    if (readYet.value === 'read') {
+        readStatus = 'read';
+    } else if (readYet.value === 'not-read') {
+        readStatus = 'not read yet';
+    }
+    // create a new Book object with this new book's properties
+    let newBook = new Book(newTitle, newAuthor, newPages, readStatus);
+    // add the new Book object to the myLibrary array
+    myLibrary.push(newBook);
+    // update the display
+    displayBooks();
+};
 
 
 // DOMContentLoaded
 
 document.addEventListener('DOMContentLoaded', (event) => {
+
+    // get variables 
     const modal = document.getElementById('myModal');
     const newBookBtn = document.getElementById('new-book-btn');
     const span = document.getElementById('close-modal');
     const addBookBtn = document.getElementById('add-book-btn');
     const form = document.getElementById('form');
+
+    // prevent form submission refreshing page
     function handleForm(event) {
         event.preventDefault();
     }
     form.addEventListener('submit', handleForm);
+
     // print books to page
     displayBooks();
 
-    // Open model when "New book" button clicked
+    // Open modal when "New book" button clicked
     newBookBtn.addEventListener("click", function() {
         modal.style.display = "block";
     });
@@ -65,15 +91,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     // Add new book to library when "Add book" button clicked
-    addBookBtn.addEventListener("click", function() {
-        let newTitle = document.getElementById('book-title').value;
-        let newAuthor = document.getElementById('book-author').value;
-        let newPages = document.getElementById('book-pages').value;
-        let readYet = document.getElementById('book-read').value;
-        let newBook = new Book(newTitle, newAuthor, newPages, readYet);
-        addBookToLibrary(newBook);
-  
-        displayBooks();
-    });
+    addBookBtn.addEventListener("click", addBook);
+
+ 
 
 });
